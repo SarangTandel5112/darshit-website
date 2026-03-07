f5c61b6f1a87701398f7d45e5126ec7fbb06291e
SarangTandel5112 <sarangtandel5112@gmail.com>
2026-03-07 12:33:26 +0530

    [Add] : LinkedIn added

--- src/data/pavanity-data.ts
+++ src/data/pavanity-data.ts
@@ -274,7 +274,7 @@ export const socialMedia = [
     { name: "WhatsApp", url: "https://wa.me/message/H6DRDTCA6OHHG1", icon: "whatsapp" },
     { name: "Instagram", url: "https://www.instagram.com/pavanity_global?igsh=Y2ZvMnJnMmM2aGRi", icon: "instagram" },
     { name: "Facebook", url: "https://facebook.com/pavanityglobal", icon: "facebook" },
-    { name: "LinkedIn", url: "https://linkedin.com/company/pavanityglobal", icon: "linkedin" },
+    { name: "LinkedIn", url: "https://www.linkedin.com/company/pavanity-global/", icon: "linkedin" },
     { name: "Twitter", url: "https://twitter.com/pavanityglobal", icon: "twitter" }
 ];
 
