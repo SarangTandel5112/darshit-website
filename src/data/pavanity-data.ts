82fcf5615e164a22f74d9b08a920c0766e92517f
Ankita Lathiagara <ankita.lathigara1@gmail.com>
2026-03-13 00:08:59 +0530

    {facebook remove icon}

--- src/data/pavanity-data.ts
+++ src/data/pavanity-data.ts
@@ -273,7 +273,7 @@ export const footerContent = {
 export const socialMedia = [
     { name: "WhatsApp", url: "https://wa.me/message/H6DRDTCA6OHHG1", icon: "whatsapp" },
     { name: "Instagram", url: "https://www.instagram.com/pavanity_global?igsh=Y2ZvMnJnMmM2aGRi", icon: "instagram" },
-    { name: "Facebook", url: "https://facebook.com/pavanityglobal", icon: "facebook" },
+    // { name: "Facebook", url: "https://facebook.com/pavanityglobal", icon: "facebook" },
     { name: "LinkedIn", url: "https://www.linkedin.com/company/pavanity-global/", icon: "linkedin" },
     { name: "Twitter", url: "https://twitter.com/pavanityglobal", icon: "twitter" }
 ];
